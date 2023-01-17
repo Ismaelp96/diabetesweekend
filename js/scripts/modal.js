d6cc24b89fa0b983a34b94721fafe74a4d8d18f2
Ismael Oliveira <ismael6991@live.com>
2023-01-16 21:45:24 -0300

    ufa modal loop

--- js/scripts/modal.js
+++ js/scripts/modal.js
@@ -1,3 +1,30 @@
-const btnModal = document.querySelector('.js-open-modal')
-const btnCloseModal = document.querySelector('.js-close-icon')
-const allModal = document.querySelector('.js-modal')
+const btnModal = document.querySelectorAll('.js-open-modal')
+const btnCloseModal = document.querySelectorAll('.js-close-icon')
+const overlayClose = document.querySelectorAll('.js-overlay-close')
+const modal = document.querySelectorAll('.js-modal')
+
+btnModal.forEach((open, index) => {
+  open.addEventListener('click', () => {
+    modal[index].classList.add('active')
+  })
+})
+
+const modalForEach = () => {
+  modal.forEach((element) => {
+    element.classList.remove('active')
+  })
+}
+
+btnCloseModal.forEach((element) => {
+  element.onclick = function (e) {
+    e.stopPropagation(e)
+    modalForEach()
+  }
+})
+
+overlayClose.forEach((element) => {
+  element.onclick = function (e) {
+    e.stopPropagation(e)
+    modalForEach()
+  }
+})
